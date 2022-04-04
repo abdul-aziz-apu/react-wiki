f3531e31e3bceb74dfc608077a6a1fe2dce0aa27
Apu <abdulazizapu@gmail.com>
2022-04-04 13:58:51 +0600

    Set dynamic routing for profiles

--- react-wiki/src/components/Card/Card.js
+++ react-wiki/src/components/Card/Card.js
@@ -1,9 +1,12 @@
 // import React from 'react'
+import { Link } from "react-router-dom";
 
 import styles from "./Card.module.scss";
 
 
-const Card = ( {results} ) => {
+
+const Card = ( { page, results } ) => {
+
     let display;
     
     if (results) {
@@ -11,7 +14,13 @@ const Card = ( {results} ) => {
             let { id, image, name, /*status*/ location } = x;
 
             return (
-                <div key={id} className="col-lg-4 col-md-6 col-sm-6 mb-4 position-relative text-dark">
+                <Link
+                style={{ textDecoration: "none" }}
+                to={`${page}${id}`}
+                key={id}
+                className="col-lg-4 col-md-6 col-sm-6 col-12 mb-4 position-relative text-dark"
+                >
+                    <div key={id} className="col-lg-4 col-md-6 col-sm-6 mb-4 position-relative text-dark">
                     <div className={`${styles.card} d-flex flex-column justify-content-center`}>
                             <img className={`${styles.img} img-fluid`} src={image} alt="" />
                     <div className={`${styles.content}`}>
@@ -21,10 +30,13 @@ const Card = ( {results} ) => {
                             <div className="fs-5">{location.name}</div>
                         </div>
                     </div>
+                   {/* Status not working */}
                 </div>
                 </div>
 
-                // Status not working
+                
+                </Link>
+                
 
             )
         })
